f8b2463207f823b573894fe42f0ee853abb97c1f
Luis <Hairchevere@gmail.com>
2022-12-08 12:55:09 -0600

    added write function to index

--- Develop/index.js
+++ Develop/index.js
@@ -50,7 +50,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("You've done it! Your README.md file has been created!!")
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
