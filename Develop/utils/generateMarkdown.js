56de3f59c89b4bd1104ff7576eb9ecb98ff9a903
Luis <Hairchevere@gmail.com>
2022-12-08 21:58:36 -0600

    added color to badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,7 +16,7 @@ function generateMarkdown(answers) {
   
   <h1 align="center">#${answers.title}</h1>
 
-  ![badge](https://img.shields.io/badge/license-${answers.license})<br />
+  ![badge:](https://img.shields.io/badge/license-${answers.license}-yellow)<br />
 
   ## Table of Contents
   - [Description](#description)
